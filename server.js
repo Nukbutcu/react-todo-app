8beca85ea4a671dd20e318c1b9b93a597eddbfe0
Nils Kratochwil <n.kratochwil@posteo.de>
2022-01-26 16:47:36 +0100

    feature: read and write from database

--- server.js
+++ server.js
@@ -1,17 +1,34 @@
 import express from "express";
+import { readFile, writeFile } from "fs/promises";
+import { v4 as uuid } from "uuid";
 
 const app = express();
 const port = 1337;
+const DATABASE_URI = "./database/database.json";
 
-app.get("/", (request_, response_) => {
-	response_.send("Hello World!");
+app.use(express.json());
+
+app.get("/", (request, response) => {
+	response.send("Hello World!");
 });
 
-app.get("/api/todos", (request_, response_) => {
-	response_.json([
-		{ id: 1, name: "clean room" },
-		{ id: 2, name: "clean room" },
-	]);
+app.get("/api/todos", async (request, response) => {
+	const data = await readFile(DATABASE_URI, "utf8");
+	const json = JSON.parse(data);
+	console.log(data);
+
+	response.json(json.todos);
+});
+
+app.post("/api/todos", async (request, response) => {
+	const data = await readFile(DATABASE_URI, "utf8");
+	const json = JSON.parse(data);
+
+	const todo = { ...request.body, id: uuid() };
+	json.todos.push(todo);
+	await writeFile(DATABASE_URI, JSON.stringify(json));
+	response.status(201);
+	response.json(todo);
 });
 
 app.listen(port, () => {
